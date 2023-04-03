daebf61c1c5c31ca5d7de8a501cd20cd0938f454
Devandra <dealfatahillahdevandra@gmail.com>
2023-04-03 11:11:21 +0700

    membuat 1 view tugas praktik pwl

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\AdminController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/daftar_mahasiswa','App\Http\Controllers\MahasiswaController@index');
+
+Route::get('/daftar_mahasiswa', [MahasiswaController::class,'index']);
+
+// Route::post('detail_mahasiswa',[MahasiswaController::class, 'detail_mhs']);
+
+Route::match(['get', 'post'], '/detail_mahasiswa', [MahasiswaController::class, 'detail_mhs']);
+ 
+Route::get('/admin/dashboard',[AdminController::class,'index']);
+Route::get('/admin/postingan',[AdminController::class,'show_postingan']);
+Route::get('/admin/arsip',[AdminController::class,'show_arsip']);
+Route::get('/admin/user',[AdminController::class,'show_user']);
